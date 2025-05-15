551c2e7315e7033cae6c5bcfd3f7863e743080be
Pablo Guzman <pmartinez08@alumnos.uaq.mx>
2025-05-14 22:20:34 -0600

    TicTacToe partial solution

---
+++ 0.Search/tictactoe/tictactoe.py
@@ -0,0 +1,102 @@
+"""
+Tic Tac Toe Player
+"""
+
+import math
+import copy
+
+X = "X"
+O = "O"
+EMPTY = None
+
+def initial_state():
+    """
+    Returns starting state of the board.
+    """
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
+
+def player(board):
+    """
+    Returns player who has the next turn on a board.
+    """
+    count_X = 0
+    count_O = 0
+
+    for row in board:
+        for col in row:
+            if col == X:
+                count_X += 1
+            if col == O:
+                count_O += 1
+    
+    if count_X > count_O:
+        return O
+    else:
+        return X
+
+def actions(board):
+    """
+    Returns set of all possible actions (i, j) available on the board.
+    """
+    possible_actions = set()
+    for row in range(3):
+        for col in range(3):
+            if board[row][col] == EMPTY:
+                possible_actions.add((row, col))
+    return possible_actions
+
+def result(board, action):
+    """
+    Returns the board that results from making move (i, j) on the board.
+    """
+    if action not in actions(board):
+        raise ValueError('Invalid Move!')
+    
+    result_board = copy.deepcopy(board)
+
+    mover = player(board)
+    (i, j) = action
+    result_board[i][j] = mover
+
+    return result_board
+
+def winner(board):
+    """
+    Returns the winner of the game, if there is one.
+    """
+
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] != EMPTY:
+            return board[i][0]    
+        
+    for j in range(3):
+        if board[0][j] == board[1][j] == board[2][j] != EMPTY:
+            return board[0][j]
+
+    if board[0][0] == board[1][1] == board[2][2] != EMPTY:
+        return board[0][0]    
+    if board[0][2] == board[1][1] == board[2][0] != EMPTY:
+        return board[0][2]    
+    
+    return None
+
+
+def terminal(board):
+    """
+    Returns True if game is over, False otherwise.
+    """
+    raise NotImplementedError
+
+def utility(board):
+    """
+    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
+    """
+    raise NotImplementedError
+
+def minimax(board):
+    """
+    Returns the optimal action for the current player on the board.
+    """
+    raise NotImplementedError
